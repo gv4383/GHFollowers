39ba5f83f1ba6c8f26f9d720b6f8427ee17fb528
gv4383 <gregory.vargas18@gmail.com>
2022-06-22 09:37:55 -0500

    Create custom button and text field components

---
+++ GHFollowers/Components/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Goyo Vargas on 6/22/22.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        // setting frame to 0 since we can change frame with auto layout
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
