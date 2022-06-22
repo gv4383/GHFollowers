1e0ec27c7c948e55ed9bf0246234df5850c0db33
gv4383 <gregory.vargas18@gmail.com>
2022-06-22 09:37:28 -0500

    Create tab bar

---
+++ GHFollowers/ViewControllers/FavoritesListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Goyo Vargas on 6/22/22.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
--- GHFollowers/ViewControllers/SearchViewController.swift
+++ GHFollowers/ViewControllers/SearchViewController.swift
@@ -1,19 +1,15 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GHFollowers
 //
-//  Created by Goyo Vargas on 6/21/22.
+//  Created by Goyo Vargas on 6/22/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .systemPink
-    }
-
-
-}
-
+    }}
