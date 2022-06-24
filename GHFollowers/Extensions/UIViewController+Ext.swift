406bfa338af8dc0fc1d2a9f5bf5a7bf4ef5732fa
gv4383 <gregory.vargas18@gmail.com>
2022-06-24 10:38:48 -0500

    Create reusable, custom alert component

---
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Goyo Vargas on 6/24/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertViewController = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertViewController.modalPresentationStyle = .overFullScreen
+            alertViewController.modalTransitionStyle = .crossDissolve
+            self.present(alertViewController, animated: true)
+        }
+    }
+}
--- GHFollowers/Views/Screens/SearchViewController.swift
+++ GHFollowers/Views/Screens/SearchViewController.swift
@@ -40,7 +40,7 @@ class SearchViewController: UIViewController {
     
     @objc func pushFollowerListViewController() {
         guard isUsernameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for.", buttonTitle: "Ok")
             return
         }
         
